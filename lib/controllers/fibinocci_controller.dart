615a714eec9a6371f8bddd45b56130375ac4d427
Ashiquekk696 <ashiquesofdevoloper@gmail.com>
2024-04-16 00:26:29 +0530

    mv

--- lib/controllers/fibinocci_controller.dart
+++ lib/controllers/fibinocci_controller.dart
@@ -1,11 +1,11 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'dart:core';
 
 import 'package:get/get_rx/get_rx.dart';
 
+//1,1,2,3,5,8,13,21,34,55, ....
 class FibinocciController extends GetxController {
   @override
   void onInit() {
@@ -21,26 +21,43 @@ class FibinocciController extends GetxController {
   late var isLoading = false.obs;
   Future<void> findMyFibinocci(BigInt x, BigInt y, c, {d}) async {
     isLoading.value = true;
-    bigData.value.clear();
-    await fibinocci(x, y, c, d: d);
+    if (d == "0") {
+      Fluttertoast.showToast(msg: "Sorry !!! 0 can't be an input");
+    } else {
+      bigData.value.clear();
+      bigData.value.insert(0,BigInt.parse("0"));
 
-    print(bigData.length);
+      await fibinocci(x, y, c, d: d);
 
-    isLoading.value = false;
+      print(bigData.length);
+
+      isLoading.value = false;
+    }
+    print("bigdata ${bigData.value}");
   }
 
   Future<void> fibinocci(BigInt x, BigInt y, c, {d}) async {
-    if (c <= num.parse(d)) {
+    if (c < num.parse(d)) {
       BigInt z = x + y;
       c++;
-      await fibinocci(y, z, c, d: d);
-      
+
+      print("x is $x");
+      if(d!="1"){
+
       bigData.value.add(y);
+
+      await fibinocci(y, z, c, d: d);
+      }
       print("y ${y}");
     }
     if (bigData.value.isNotEmpty) {
-      fibinocciValue.value = bigData.value.first.toString();
+      fibinocciValue.value = bigData.value.last.toString();
     }
+    if (x == 0) {
+      print("x is $x");
+      bigData.value.add(x);
+    }
+
     update();
   }
 }
